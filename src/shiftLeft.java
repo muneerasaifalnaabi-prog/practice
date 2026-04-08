8d19552b33fe817c9ca8c96ec387c6998945fa9f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-08 11:10:33 +0400

    Task 9: shiftLeft : completed

---
+++ src/shiftLeft.java
@@ -0,0 +1,25 @@
+public class shiftLeft {
+    public static void main(String[] args) {
+        int[] nums = {6, 2, 5, 3};
+        int[] arr = shiftLeft(nums);
+
+        // Print each element of the shifted array
+        for (int n : arr) {
+            System.out.println(n);
+        }
+    }
+
+    public static int[] shiftLeft(int[] nums) {
+        int[] arr = new int[nums.length];
+
+        if (nums.length == 0)
+            return arr;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            arr[i] = nums[i + 1];
+        }
+
+        arr[nums.length - 1] = nums[0];
+        return arr;
+    }
+}
